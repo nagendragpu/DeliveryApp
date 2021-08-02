dfa04eb5ea79d2ee36e443ccfdbe10ca01b4a0b0
nagendragpu <nagendragpu@gmail.com>
2021-08-02 16:24:10 +0530

    DrawerScreen

--- navigation/CustomDrawer.js
+++ navigation/CustomDrawer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View, Image } from "react-native";
 import {
   createDrawerNavigator,
   DrawerContentScrollView,
@@ -15,6 +15,125 @@ import {
 } from "../constants";
 
 const Drawer = createDrawerNavigator();
+
+const CustomDrawerItem = ({ label, icon }) => {
+  return (
+    <TouchableOpacity
+      style={{
+        flexDirection: "row",
+        height: 40,
+        marginBottom: SIZES.base,
+        alignItems: "center",
+        paddingLeft: SIZES.radius,
+        borderRadius: SIZES.base,
+      }}
+    >
+      <Image
+        source={icon}
+        style={{
+          height: 20,
+          width: 20,
+          tintColor: COLORS.white,
+        }}
+      />
+      <Text
+        style={{
+          marginLeft: 15,
+          color: COLORS.white,
+          ...FONTS.h3,
+        }}
+      >
+        {label}
+      </Text>
+    </TouchableOpacity>
+  );
+};
+
+const CustomDrawerContent = ({ navigation }) => {
+  return (
+    <DrawerContentScrollView
+      scrollEnabled={true}
+      contentContainerStyle={{ flex: 1 }}
+    >
+      <View style={{ flex: 1, paddingHorizontal: SIZES.radius }}>
+        <View style={{ alignItems: "flex-start", justifyContent: "center" }}>
+          <TouchableOpacity
+            style={{ alignItems: "center", justifyContent: "center" }}
+            onPress={() => navigation.closeDrawer()}
+          >
+            <Image
+              source={icons.cross}
+              style={{ height: 35, width: 35, tintColor: COLORS.white }}
+            />
+          </TouchableOpacity>
+        </View>
+
+        <TouchableOpacity
+          style={{
+            flexDirection: "row",
+            marginTop: SIZES.radius,
+            alignItems: "center",
+          }}
+          onPress={() => console.log("Profile")}
+        >
+          <Image
+            source={dummyData.myProfile?.profile_image}
+            style={{ height: 50, width: 50, borderRadius: SIZES.radius }}
+          />
+
+          <View style={{ marginLeft: SIZES.radius }}>
+            <Text style={{ color: COLORS.white, ...FONTS.h3 }}>
+              {dummyData.myProfile?.name}
+            </Text>
+            <Text style={{ color: COLORS.white, ...FONTS.body4 }}>
+              View your profile
+            </Text>
+          </View>
+        </TouchableOpacity>
+        {/* Drawer Items */}
+
+        <View style={{ flex: 1, marginTop: SIZES.padding }}>
+          <CustomDrawerItem label={constants.screens.home} icon={icons.home} />
+          <CustomDrawerItem
+            label={constants.screens.my_wallet}
+            icon={icons.wallet}
+          />
+          <CustomDrawerItem
+            label={constants.screens.notification}
+            icon={icons.notification}
+          />
+          <CustomDrawerItem
+            label={constants.screens.favourite}
+            icon={icons.favourite}
+          />
+          <View
+            style={{
+              height: 1,
+              marginVertical: SIZES.radius,
+              marginLeft: SIZES.radius,
+              backgroundColor: COLORS.lightGray1,
+            }}
+          ></View>
+          <CustomDrawerItem label="Track Your Order" icon={icons.location} />
+          <CustomDrawerItem label="Coupons" icon={icons.coupon} />
+          <CustomDrawerItem label="Settings" icon={icons.setting} />
+          <CustomDrawerItem
+            label="Invite Your Friend   "
+            icon={icons.profile}
+          />
+          <CustomDrawerItem label="Help a Center" icon={icons.help} />
+        </View>
+        <View
+          style={{
+            marginBottom: SIZES.padding,
+          }}
+        >
+          <CustomDrawerItem label="Logout" icon={icons.logout} />
+        </View>
+      </View>
+    </DrawerContentScrollView>
+  );
+};
 
 const CustomDrawer = () => {
   return (
@@ -35,6 +154,9 @@ const CustomDrawer = () => {
         }}
         sceneContainerStyle={{ backgroundColor: "transparent" }}
         initialRouteName="MainLayout"
+        drawerContent={(props) => {
+          return <CustomDrawerContent navigation={props.navigation} />;
+        }}
       >
         <Drawer.Screen name="MainLayout">
           {(props) => <MainLayout {...props} />}
--- screens/MainLayout.js
+++ screens/MainLayout.js
@@ -1,21 +1,18 @@
-import React from 'react';
-import {
-    View,
-    Text
-} from 'react-native';
+import React from "react";
+import { View, Text } from "react-native";
 
 const MainLayout = () => {
-    return (
-        <View
-            style={{
-                flex: 1,
-                alignItems: 'center',
-                justifyContent: 'center'
-            }}
-        >
-            <Text>MainLayout</Text>
-        </View>
-    )
-}
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Text>MainLayoutSCree</Text>
+    </View>
+  );
+};
 
 export default MainLayout;
