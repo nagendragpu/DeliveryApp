6cf94d6ba583286119d0a8b25d7241943a48aa0c
nagendragpu <nagendragpu@gmail.com>
2021-08-03 00:07:22 +0530

    Home SearchBar

--- screens/Home/Home.js
+++ screens/Home/Home.js
@@ -1,16 +1,60 @@
 import React from "react";
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  TextInput,
+  FlatList,
+} from "react-native";
+
+import { FONTS, SIZES, COLORS, icons, dummyData } from "../../constants";
 
 const Home = () => {
+  function renderSearch() {
+    return (
+      <View
+        style={{
+          flexDirection: "row",
+          height: 40,
+          alignItems: "center",
+          marginHorizontal: SIZES.padding,
+          marginVertical: SIZES.base,
+          paddingHorizontal: SIZES.radius,
+          borderRadius: SIZES.radius,
+          backgroundColor: COLORS.lightGray2,
+        }}
+      >
+        <Image
+          source={icons.search}
+          style={{ height: 20, width: 20, tintColor: COLORS.black }}
+        />
+        <TextInput
+          style={{
+            flex: 1,
+            marginLeft: SIZES.radius,
+            ...FONTS.body3,
+          }}
+          placeholder="search food"
+        />
+
+        <TouchableOpacity>
+          <Image
+            source={icons.filter}
+            style={{ height: 20, width: 20, tintColor: COLORS.black }}
+          />
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
   return (
     <View
       style={{
         flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
       }}
     >
-      <Text>Home</Text>
+      {renderSearch()}
     </View>
   );
 };
