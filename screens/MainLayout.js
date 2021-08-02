1ab3a1a530682d7143e2e2e68c49c13ca7b7de1e
nagendragpu <nagendragpu@gmail.com>
2021-08-02 18:37:05 +0530

    LinearGradient & Header

--- screens/MainLayout.js
+++ screens/MainLayout.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {
   View,
   Text,
@@ -12,9 +12,11 @@ import Animated, {
   useSharedValue,
   withTiming,
 } from "react-native-reanimated";
+import LinearGradient from "react-native-linear-gradient";
 import { connect } from "react-redux";
 import { setSelectedTab } from "../stores/tab/tabActions";
 import { Home, Search, CartTab, Favourite, Notification } from "../screens";
+import { Header } from "../components";
 import {
   COLORS,
   SIZES,
@@ -30,20 +32,87 @@ const MainLayout = ({
   selectedTab,
   setSelectedTab,
 }) => {
+  useEffect(() => {
+    setSelectedTab(constants.screens.home);
+  }, []);
+
   return (
     <Animated.View
       style={{
         flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: "white",
+
+        backgroundColor: COLORS.white,
         ...drawerAnimationStyle,
       }}
     >
       {/* header */}
+      <Header
+        containerStyle={{
+          height: 50,
+          paddingHorizontal: SIZES.padding,
+          marginTop: 40,
+          alignItems: "center",
+        }}
+        title={selectedTab.toUpperCase()}
+        leftComponent={
+          <TouchableOpacity
+            style={{
+              width: 40,
+              height: 40,
+              alignItems: "center",
+              justifyContent: "center",
+              borderWidth: 1,
+              borderColor: COLORS.gray2,
+              borderRadius: SIZES.radius,
+            }}
+            onPress={() => navigation.openDrawer()}
+          >
+            <Image source={icons.menu} />
+          </TouchableOpacity>
+        }
+        rightComponent={
+          <TouchableOpacity
+            style={{
+              alignItems: "center",
+              justifyContent: "center",
+
+              borderRadius: SIZES.radius,
+            }}
+          >
+            <Image
+              source={dummyData?.myProfile?.profile_image}
+              style={{ width: 40, height: 40, borderRadius: SIZES.radius }}
+            />
+          </TouchableOpacity>
+        }
+      />
+
       {/* content */}
-      <Text>MainLayoutSCree</Text>
+      <View style={{ flex: 1 }}>
+        <Text>MainLayoutSCree</Text>
+      </View>
       {/* footer */}
+      <View
+        style={{
+          height: 100,
+          justifyContent: "flex-end",
+        }}
+      >
+        <LinearGradient
+          start={{ x: 0, y: 0 }}
+          end={{ x: 0, y: 4 }}
+          colors={[COLORS.transparent, COLORS.lightGray1]}
+          style={{
+            position: "absolute",
+            top: -20,
+            left: 0,
+            right: 0,
+            height: 100,
+            borderTopLeftRadius: 15,
+            botderTopRightRadius: 15,
+          }}
+        />
+      </View>
     </Animated.View>
   );
 };
