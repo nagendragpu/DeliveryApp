4702ca3ed84dac6426c0949c96fc982a68f691ad
nagendragpu <nagendragpu@gmail.com>
2021-08-04 13:17:12 +0530

    Modal Animation

--- screens/Home/FilterModal.js
+++ screens/Home/FilterModal.js
@@ -11,13 +11,29 @@ import {
 import { FONTS, SIZES, COLORS, icons, dummyData } from "../../constants";
 
 const FilterModal = ({ isVisible, onClose }) => {
+  const modalAnimatedValue = React.useRef(new Animated.Value(0)).current;
   const [showFilterMode, setShowFilterMode] = React.useState(isVisible);
 
   React.useEffect(() => {
-    if (!showFilterMode) {
-      onClose();
+    if (showFilterMode) {
+      Animated.timing(modalAnimatedValue, {
+        toValue: 1,
+        duration: 500,
+        useNativeDriver: false,
+      }).start();
+    } else {
+      Animated.timing(modalAnimatedValue, {
+        toValue: 0,
+        duration: 500,
+        useNativeDriver: false,
+      }).start(() => onClose());
     }
   }, [showFilterMode]);
+
+  const modalY = modalAnimatedValue.interpolate({
+    inputRange: [0, 1],
+    outputRange: [SIZES.height, SIZES.height - 550],
+  });
 
   return (
     <Modal animationType="fade" transparent={true} visible={isVisible}>
@@ -40,6 +56,20 @@ const FilterModal = ({ isVisible, onClose }) => {
             }}
           ></View>
         </TouchableWithoutFeedback>
+
+        <Animated.View
+          style={{
+            position: "absolute",
+            left: 0,
+            top: modalY,
+            width: "100%",
+            height: "100%",
+            padding: SIZES.padding,
+            borderTopLeftRadius: SIZES.padding,
+            borderTopRightRadius: SIZES.padding,
+            backgroundColor: COLORS.white,
+          }}
+        ></Animated.View>
       </View>
     </Modal>
   );
